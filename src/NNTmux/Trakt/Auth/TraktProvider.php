2af995a2c19098a68be9098feb7e180f7937f9c9
DariusIII <dkrisan@gmail.com>
2018-02-21 12:35:51 +0100

    Add missing abstract functions to TraktProvider class

--- src/NNTmux/Trakt/Auth/TraktProvider.php
+++ src/NNTmux/Trakt/Auth/TraktProvider.php
@@ -1,15 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: bwubs
- * Date: 20/02/15
- * Time: 22:12
- */
-
 namespace NNTmux\Trakt\Auth;
 
 use League\OAuth2\Client\Provider\AbstractProvider;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use League\OAuth2\Client\Provider\ResourceOwnerInterface;
 use League\OAuth2\Client\Token\AccessToken;
+use Psr\Http\Message\ResponseInterface;
 
 class TraktProvider extends AbstractProvider
 {
@@ -47,9 +43,9 @@ class TraktProvider extends AbstractProvider
 
     public function getAuthorizationUrl($options = [])
     {
-        $this->state = isset($options['state']) ? $options['state'] : md5(uniqid(rand(), true));
+        $this->state = $options['state'] ?? md5(uniqid(mt_rand(), true));
         $params = [
-            'response_type' => isset($options['response_type']) ? $options['response_type'] : 'code',
+            'response_type' => $options['response_type'] ?? 'code',
             'client_id' => $this->clientId,
             'redirect_uri' => $this->redirectUri,
             'state' => $this->state
@@ -80,6 +76,84 @@ class TraktProvider extends AbstractProvider
     public function userDetails($response, AccessToken $token)
     {
     }
-
-
-}
+	
+	
+	/**
+	 * Returns the base URL for authorizing a client.
+	 *
+	 * Eg. https://oauth.service.com/authorize
+	 *
+	 * @return string
+	 */
+	public function getBaseAuthorizationUrl()
+	{
+		// TODO: Implement getBaseAuthorizationUrl() method.
+	}
+	
+	/**
+	 * Returns the base URL for requesting an access token.
+	 *
+	 * Eg. https://oauth.service.com/token
+	 *
+	 * @param array $params
+	 *
+	 * @return string
+	 */
+	public function getBaseAccessTokenUrl(array $params)
+	{
+		// TODO: Implement getBaseAccessTokenUrl() method.
+	}
+	
+	/**
+	 * Returns the URL for requesting the resource owner's details.
+	 *
+	 * @param AccessToken $token
+	 *
+	 * @return string
+	 */
+	public function getResourceOwnerDetailsUrl(AccessToken $token)
+	{
+		// TODO: Implement getResourceOwnerDetailsUrl() method.
+	}
+	
+	/**
+	 * Returns the default scopes used by this provider.
+	 *
+	 * This should only be the scopes that are required to request the details
+	 * of the resource owner, rather than all the available scopes.
+	 *
+	 * @return array
+	 */
+	protected function getDefaultScopes()
+	{
+		// TODO: Implement getDefaultScopes() method.
+	}
+	
+	/**
+	 * Checks a provider response for errors.
+	 *
+	 * @throws IdentityProviderException
+	 *
+	 * @param  ResponseInterface $response
+	 * @param  array|string      $data Parsed response data
+	 *
+	 * @return void
+	 */
+	protected function checkResponse(ResponseInterface $response, $data)
+	{
+		// TODO: Implement checkResponse() method.
+	}
+	
+	/**
+	 * Generates a resource owner object from a successful resource owner
+	 * details request.
+	 *
+	 * @param  array       $response
+	 * @param  AccessToken $token
+	 *
+	 * @return ResourceOwnerInterface
+	 */
+	protected function createResourceOwner(array $response, AccessToken $token)
+	{
+		// TODO: Implement createResourceOwner() method.
+}}
